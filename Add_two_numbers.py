7cdda862ffb49ccb7079b6920b37bcbb656317dc
Daniel Qi <527633059@qq.com>
2017-01-19 11:15:42 +0800

    Update Add_two_numbers.py

--- Add_two_numbers.py
+++ Add_two_numbers.py
@@ -1,1 +1,22 @@
+# Definition for sigly-linked list.
+# class ListNode(object):
+#   def __init__(self, x):
+#       self.val=x
+#       self.next=None
 
+class Solution(object):
+   def addTwoNumbers(self,l1,l2):
+      nHead,flag=ListNode(0),0
+      head=nHead
+      while flag or l1 or l2:
+        node=ListNode(flag)
+        if l1:
+          node.val += l1.val
+          l1=l1.next
+        if l2:
+          node.val += l2.val
+          l2=l2.next
+        flag=node // 10
+        node.val %= 10
+        head.next, head=node, node
+      return nHead.next
