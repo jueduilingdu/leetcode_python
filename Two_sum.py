0742f83938d9d286f849daa178d2c28f3600cfd2
Daniel Qi <527633059@qq.com>
2017-01-19 10:38:00 +0800

    Create Two_sum.py

---
+++ Two_sum.py
@@ -0,0 +1,8 @@
+Class Solutin(object):
+  def twoSum(self, nums, target):
+    dict={}
+    for i in range(length(nums)):
+      if dict.get(target-nums[i],None)==None:
+        dict[nums[i]]=i
+      else:
+        return (dict[target-nums[i]],i)
