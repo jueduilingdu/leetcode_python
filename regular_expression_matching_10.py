3c47d9e30a5ca2acae424d13899baca5b3a31844
Daniel Qi <527633059@qq.com>
2017-02-25 20:52:28 +0800

    Update regular_expression_matching_10.py

--- regular_expression_matching_10.py
+++ regular_expression_matching_10.py
@@ -25,18 +25,16 @@ class Solution(object):
     # @return a boolean
     # DP算法：http://blog.csdn.net/fzzying3/article/details/42057935；http://blog.csdn.net/u014265088/article/details/52574639
     def isMatch(self, s, p):
-        dp=[[False for i in range(len(p)+1)] for j in range(len(s)+1)]
-        dp[0][0]=True
-        for i in range(1,len(p)+1):
-            if p[i-1]=='*':
-                if i>=2:
-                    dp[0][i]=dp[0][i-2]
-        for i in range(1,len(s)+1):
-            for j in range(1,len(p)+1):
-                if p[j-1]=='.':
-                    dp[i][j]=dp[i-1][j-1]
-                elif p[j-1]=='*':
-                    dp[i][j]=dp[i][j-1] or dp[i][j-2] or (dp[i-1][j] and (s[i-1]==p[j-2] or p[j-2]=='.'))
-                else:
-                    dp[i][j]=dp[i-1][j-1] and s[i-1]==p[j-1]
-        return dp[len(s)][len(p)]
+        s, p = ' ' + s, ' ' + p
+        dp = [[False] * (len(p)) for i in range(len(s))]
+        dp[0][0] = True
+        ind = 2
+        while ind < len(p) and p[ind] == '*':
+            dp[0][ind], ind = True, ind + 2
+        for i in range(1, len(s)):
+            for j in range(1, len(p)):
+                if (s[i] == p[j] or p[j] == '.') and dp[i-1][j-1]:
+                    dp[i][j] = True
+                if p[j] == '*' and (dp[i][j-2] or ((p[j-1] == '.' or p[j-1] == s[i]) and (dp[i-1][j-2] or dp[i-1][j]))):
+                    dp[i][j] = True
+        return dp[len(s) - 1][len(p) - 1]
